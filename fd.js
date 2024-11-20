0385f97d03c461b20967fc28bcb6d1a0600ca1d5
fahad <fahadnb720@gmail.com>
2024-11-20 14:04:04 +0530

    pen

--- fd.js
+++ fd.js
@@ -1,5 +1,10 @@
 const card = document.getElementsByClassName("cards")
-const cards= card.addEventListener('mouseOver' , () => {
+    const cards= card.addEventListener('mouseOver' , function() => {
     card.style.transform = 'scale(1.05)'; 
     card.style.transition = 'transform 0.3s ease-in-out'; 
-})
+
+    cards.addEventListener('mouseout', function() {
+        this.style.transform = 'scale(1)';  // Reset the scale
+        this.style.boxShadow = 'none';  
+    });// Remove shadow
+});
--- fdrm.js
+++ fdrm.js
@@ -1,10 +1,11 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 const cards = document.querySelectorAll('.cards');
-    cards.forEach(cards => {4
+    cards.forEach(cards => {
         cards.addEventListener('mouseover', function() {
             this.style.transform = 'scale(1.05)';  // Slightly enlarge the card
             this.style.transition = 'transform 0.3s ease-in-out';  // Smooth transition
+            this.style.boxShadow = '0 4px 15px rgba(0,0,0,0.4)'
         });
 
         cards.addEventListener('mouseout', function() {
@@ -12,4 +13,8 @@ const cards = document.querySelectorAll('.cards');
             this.style.boxShadow = 'none';  // Remove shadow
         });
     });
-});
+const loginLink=document.querySelector('.menu-button,li:nth-child(1)');
+    loginLink.addEventListener('click', function(event){
+        alert('WELCOME TO THE NEXT PAGE!!');
+    });    
+});    
