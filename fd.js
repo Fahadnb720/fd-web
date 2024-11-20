e36d15a4d31500f41b882c51baf8133062993bce
fahad <fahadnb720@gmail.com>
2024-11-20 15:08:43 +0530

    pending2

--- fd.js
+++ fd.js
@@ -1,5 +1,5 @@
 const card = document.getElementsByClassName("cards")
-    const cards= card.addEventListener('mouseOver' , function() => {
+    const cards= card.addEventListener('mouseOver' , function() {
     card.style.transform = 'scale(1.05)'; 
     card.style.transition = 'transform 0.3s ease-in-out'; 
 
--- fdrm.js
+++ fdrm.js
@@ -3,18 +3,32 @@ document.addEventListener('DOMContentLoaded', function() {
 const cards = document.querySelectorAll('.cards');
     cards.forEach(cards => {
         cards.addEventListener('mouseover', function() {
-            this.style.transform = 'scale(1.05)';  // Slightly enlarge the card
-            this.style.transition = 'transform 0.3s ease-in-out';  // Smooth transition
+            this.style.transform = 'scale(1.05)';  
+            this.style.transition = 'transform 0.3s ease-in-out';  
             this.style.boxShadow = '0 4px 15px rgba(0,0,0,0.4)'
         });
 
         cards.addEventListener('mouseout', function() {
-            this.style.transform = 'scale(1)';  // Reset the scale
-            this.style.boxShadow = 'none';  // Remove shadow
+            this.style.transform = 'scale(1)';  
+            this.style.boxShadow = 'none';  
         });
     });
 const loginLink=document.querySelector('.menu-button,li:nth-child(1)');
     loginLink.addEventListener('click', function(event){
         alert('WELCOME TO THE NEXT PAGE!!');
+
+const anime=document.querySelector('.menu-button,li:nth-child(3)');
+    anime.forEach(anime =>{
+        anime.addEventListener('mouseover', function() {
+            this.style.transform = 'scale(1.05)'; 
+            this.style.transition = 'transform 0.3s ease-in-out';  
+            this.style.boxShadow = '0 4px 15px rgba(0,0,0,0.4)'
+        });
+
+        cards.addEventListener('mouseout', function() {
+            this.style.transform = 'scale(1)'; 
+            this.style.boxShadow = 'none';  
+        });
+    });
     });    
 });    
